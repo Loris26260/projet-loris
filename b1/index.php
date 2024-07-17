eda24be50a1b723f4c514f7faef378d7d9690ce4
Loris <lorislovetdurbet@gmail.com>
2024-07-17 11:13:53 +0200

    4commit

--- b1/index.php
+++ b1/index.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-// Connexion à la base de données
-$host = 'localhost';
-$dbname = 'pharmacie';
-$user = 'root';
-$pass = '';
+// Connexion à la base de données MySQL sur un serveur Oracle
+$host = '144.24.194.165/'; // Adresse du serveur Oracle
+$dbname = 'Pharmacie';
+$user = 'mysql_user'; // Nom d'utilisateur MySQL
+$pass = 'mysql_password'; // Mot de passe MySQL
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = htmlspecialchars($_POST['password']);
 
     // Préparer la requête pour vérifier les informations d'identification
-    $stmt = $pdo->prepare("SELECT * FROM user WHERE username = ?");
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
     $stmt->execute([$username]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
